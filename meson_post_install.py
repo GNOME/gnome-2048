84ad3134b52307874b5c7fac931157024a093243
Jan Tojnar <jtojnar@gmail.com>
2019-01-17 04:52:29 +0100

    build: Make post install script more portable
    When datadir is set to non-defautl value, the post install script will not work
    as intended. Additionally, we no longer rely on shebang, which is not portable
    either. (/usr/bin/env is not used on Nix, for example.)

--- meson_post_install.py
+++ meson_post_install.py
@@ -2,13 +2,17 @@
 
 import os
 import subprocess
+import sys
+
+
+if len(sys.argv) < 3:
+  sys.exit("usage: meson_post_install.py <icondir> <schemadir>")
+
+icon_cache_dir = sys.argv[1]
+schemadir = sys.argv[2]
 
 if not os.environ.get('DESTDIR'):
-  prefix = os.environ.get('MESON_INSTALL_PREFIX', '/usr/local')
-  datadir = os.path.join(prefix, 'share')
-
   print('Updating icon cache...')
-  icon_cache_dir = os.path.join(datadir, 'icons', 'hicolor')
   if not os.path.exists(icon_cache_dir):
     os.makedirs(icon_cache_dir)
   subprocess.call(['gtk-update-icon-cache',
@@ -16,7 +20,6 @@ if not os.environ.get('DESTDIR'):
                    icon_cache_dir])
 
   print('Compiling GSettings schemas...')
-  schemas_dir = os.path.join(datadir, 'glib-2.0', 'schemas')
-  if not os.path.exists(schemas_dir):
-    os.makedirs(schemas_dir)
-  subprocess.call(['glib-compile-schemas', schemas_dir])
+  if not os.path.exists(schemadir):
+    os.makedirs(schemadir)
+  subprocess.call(['glib-compile-schemas', schemadir])
